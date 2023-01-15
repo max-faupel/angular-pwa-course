cceb63a499d962579b63fd8a0e11937b69e7233c
Max Faupel <max.faupel@gmx.de>
2023-01-15 10:01:36 +0000

    enable service worker

---
+++ COMMANDS.md
@@ -0,0 +1,8 @@
+```
+npm install -save @angular/service-worker
+ng build --prod
+npm run server
+npm install -g http-server
+http-server -c-1 -P http://localhost:9000 .
+npm run start:prod
+```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import {AppComponent} from './app.component';
-import {LessonsComponent} from './lessons/lessons.component';
+import { AppComponent } from './app.component';
+import { LessonsComponent } from './lessons/lessons.component';
 
-import {LessonsService} from "./services/lessons.service";
-import {ReactiveFormsModule} from "@angular/forms";
+import { LessonsService } from "./services/lessons.service";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import {environment} from '../environments/environment.prod';
-import {ServiceWorkerModule} from '@angular/service-worker';
-
-
+import { environment } from '../environments/environment.prod';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 
@@ -19,28 +17,31 @@ import {ServiceWorkerModule} from '@angular/service-worker';
 
 
 
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {AppRoutingModule} from "./app-routing.module";
+
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./app-routing.module";
 
 
 
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        LessonsComponent
-    ],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        AppRoutingModule,
-        ReactiveFormsModule
-    ],
-    providers: [
-        LessonsService
-    ],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    LessonsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [
+    LessonsService
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 
